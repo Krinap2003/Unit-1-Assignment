e450328a1e34761372afe03bcac26768ffc2acbf
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-09-17 20:32:07 -0400

    unit 2 intial commit

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Krina Patel on 9/17/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
